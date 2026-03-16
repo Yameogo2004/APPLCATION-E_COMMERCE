9c38724f87e46b6239e439e31f5c85c6a07a38c7
YAMEOGO Ariel Barthelemy Wendtoin <arielyameogo2004@gmail.com>
2026-03-16 20:34:53 +0000

    Add files via upload

---
+++ OrderItem.java
@@ -0,0 +1,50 @@
+package e_commerce;
+
+public class OrderItem {
+
+    private int id;
+    private Order order;
+    private Product product;
+    private int quantity;
+    private double unitPrice;
+
+    public OrderItem(int id, Order order, Product product, int quantity, double unitPrice) {
+        this.id = id;
+        this.order = order;
+        this.product = product;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double calculSubtotal() {
+        return quantity * unitPrice;
+    }
+}
